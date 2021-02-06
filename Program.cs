bf28f708de2fa403880046d6a2abce8bb871d016
jeanj <jeanj@DESKTOP-HNJ5TV3>
2021-02-05 22:15:08 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ArrayProject
+{
+    class Program
+    {
+        private static int pop_array;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array of 25 integers");
+
+            
+                int[] numbersArray = new int[25];
+
+                for (int counter=0;counter<25;counter++)
+                {
+                    numbersArray[counter] = counter;
+                }
+
+                for (int i=0; i<25; i++)
+                 {
+                    numbersArray[i] = pop_array;
+                    Console.Write( i + "");
+                 }
+            
+                
+            
+
+
+
+
+            
+            }
+        }
+    }
+
